39241bfff5020a16fe3d2ce1e39f4283f7087e8d
Vlad Khazin <vladimir.khazin@icssolutions.ca>
2020-02-07 20:40:47 +0000

    correctingsetup

--- setup-cron-job.sh
+++ setup-cron-job.sh
@@ -3,4 +3,5 @@ mkdir -p /opt/snapshot-copy
 cp ./copy-snapshot.sh /opt/snapshot-copy
 cp ./cron-job.sh /opt/snapshot-copy
 SCHEDULE='*/1 * * * *' #https://crontab.guru/
-echo "$SCHEDULE root /opt/shapshot-copy/cron-job.sh" | tee /etc/crond./snapshot-copy
+echo "$SCHEDULE root /opt/shapshot-copy/cron-job.sh" | tee /etc/cron.d/snapshot-copy
+echo "cron job has been configured"
